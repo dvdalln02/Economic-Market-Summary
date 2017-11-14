a33caa1824a73e9e9fdf3a00f69da8481fd49af0
dvdalln02 <dvdalln02@gmail.com>
2017-11-14 14:50:17 -0800

    end of first day

--- Economic-Market-Summary/app.R
+++ Economic-Market-Summary/app.R
@@ -1,21 +1,9 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- shinyUI(fluidPage(
+ui <- shinyUI(fluidPage(theme = 'bootstrap.css',
    
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("Economic & Market Summary"),
    
-   # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
          sliderInput("bins",
@@ -25,26 +13,23 @@ ui <- shinyUI(fluidPage(
                      value = 30)
       ),
       
-      # Show a plot of the generated distribution
       mainPanel(
          plotOutput("distPlot")
       )
    )
 ))
 
-# Define server logic required to draw a histogram
+
 server <- shinyServer(function(input, output) {
    
    output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
       x    <- faithful[, 2] 
       bins <- seq(min(x), max(x), length.out = input$bins + 1)
       
-      # draw the histogram with the specified number of bins
       hist(x, breaks = bins, col = 'darkgray', border = 'white')
    })
 })
 
-# Run the application 
+
 shinyApp(ui = ui, server = server)
 
